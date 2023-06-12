cfd0d128f7a65091f96ca6e4b91fb8610d78a9a3
victorzix <linkgamesplays@gmail.com>
2023-06-12 16:30:02 -0300

    feat: create js logic to check if the message have been read or not

--- notifications-page-main/javascript/index.js
+++ notifications-page-main/javascript/index.js
@@ -1,18 +1,30 @@
-let situation = document.querySelectorAll('[id=unread]')
-let newMessage = document.querySelectorAll('[id=contentWrapper]')
-let qtd = document.querySelector('.qtd')
+let situation = document.querySelectorAll("[id=unread]");
+let newMessage = document.querySelectorAll("[id=contentWrapper]");
+let qtd = document.querySelector(".qtd");
 
-console.log(newMessage)
 let read;
 
+for (let content of newMessage) {
+  if (content.querySelector(".unread") !== null) {
+    content.style.backgroundColor = getComputedStyle(
+      document.documentElement
+    ).getPropertyValue("--light-blue1");
+    read = false;
+  }
 
-
-qtd.textContent = 3
-function readAll(){
-    for(let div of situation){
-        div.style.display = 'none'
-        read = true;
+  qtd.textContent = 3;
+  function readAll() {
+    for (let div of situation) {
+      div.style.display = "none";
+      read = true;
     }
-    qtd.textContent = '0'
-}
 
+    setTimeout(() =>{if(read){
+        for(let content of newMessage){
+            content.style.backgroundColor = 'white'
+        }
+    }}, 300)
+    
+    qtd.textContent = "0";
+  }
+}
